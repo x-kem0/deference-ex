446abf0c0119991de41e4b3cdf11f595335aef36
x <kemo@sent.com>
2026-04-15 14:30:36 -0500

    add detail to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Deference.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Deference",
+      description: "A module for deferring cleanup functions in error cases",
+      source_url: "https://github.com/x-kem0/deference-ex"
     ]
   end
 
